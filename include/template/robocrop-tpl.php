d9d5b259bc6a234b29cbe0f891625645021bb77e
mcguffin <joern@flyingletters.com>
2016-03-19 18:51:58 +0100

    wording

--- include/template/robocrop-tpl.php
+++ include/template/robocrop-tpl.php
@@ -7,9 +7,9 @@
 		<div class="robocrop-sidebar">
 			<div class="select-ratio"></div>
 			<div class="box actions">
-				<button class="button robocrop-cancel"><?php _e('Cancel','wp-robocrop'); ?></button>
+				<button class="button robocrop-cancel"><?php _e('Close','wp-robocrop'); ?></button>
 				<# if ( data.focuspoint ) { #>
-				<button class="button robocrop-autocrop"><?php _e('Auto crop','wp-robocrop'); ?></button>
+				<button class="button robocrop-autocrop"><?php _e('Auto','wp-robocrop'); ?></button>
 				<# } #>
 				<button class="button-primary robocrop-save" disabled="disabled"><?php _e('Save','wp-robocrop'); ?></button>
 			</div>
