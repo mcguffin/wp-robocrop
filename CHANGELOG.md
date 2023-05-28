a8bec4a4f3b5359f686f087070ac2c0c93005264
mcguffin <joern@flyingletters.com>
2023-05-28 07:45:47 +0200

    changelog, auto update implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,15 @@
 WP Robocrop Changelog
 =====================
 
+1.2.7
+-----
+ - Fix PHP 8.2 warning
+ - Setup auto update via github
+
+1.1.7
+-----
+ - Abandon changelog
+
 1.1.6
 -----
  - Introduce Changelog
--- index.php
+++ index.php
@@ -53,3 +53,21 @@ if ( is_admin() ) {
 
 	Admin\Admin::instance();
 }
+
+
+
+// Enable WP auto update
+add_filter( 'update_plugins_github.com', function( $update, $plugin_data, $plugin_file, $locales ) {
+
+	if ( ! preg_match( "@{$plugin_file}$@", __FILE__ ) ) { // not our plugin
+		return $update;
+	}
+
+	$response = wp_remote_get( $plugin_data['UpdateURI'] );
+
+	if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) > 200 ) { // response error
+		return $update;
+	}
+
+	return json_decode( wp_remote_retrieve_body( $response ), true, 512 );
+}, 10, 4 );
